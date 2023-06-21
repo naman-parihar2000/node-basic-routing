90636e0fe738afccd93d321fdc2c1559c0be6b7e
Naman Singh Parihar <87593367+naman-parihar2000@users.noreply.github.com>
2023-06-21 21:31:44 +0530

    basic mounting

--- app.js
+++ app.js
@@ -3,15 +3,27 @@ const app = express()
 
 app.use(express.json())
 
-app.use((req, res, next) => {
-    req.body.name = 'lassan ' + req.body.name;
-    next();
+// app.use((req, res, next) => {
+//     req.body.name = 'lassan ' + req.body.name;
+//     next();
+// })
+
+// app.post('/', (req, res) => {
+//     const { name } = req.body
+//     res.json(`hello ${name}`)
+// })
+
+const tourRouter = express.Router()
+
+tourRouter.get('/', (req, res) => {
+    res.json(`Tours Page`)
 })
 
-app.post('/', (req, res) => {
-    const { name } = req.body
-    res.json(`hello ${name}`)
+tourRouter.get('/:id', (req, res) => {
+    res.json(`The id for the tour selected ${req.params.id}`)
 })
+
+app.use('/tours', tourRouter)
 
 const port = 5000;
 app.listen(port, () => {
