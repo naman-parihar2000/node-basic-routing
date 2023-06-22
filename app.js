1915b07c4b8df6255df434ed56cc23edaba6ad80
Naman Singh Parihar <87593367+naman-parihar2000@users.noreply.github.com>
2023-06-22 09:39:22 +0530

    static files

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(express.static(`${__dirname}/routes`))
 
 // app.use((req, res, next) => {
 //     req.body.name = 'lassan ' + req.body.name;
