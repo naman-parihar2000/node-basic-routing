d843afd0eca6f73c36ccb1afc7493e9dbbd0552c
Naman Singh Parihar <87593367+naman-parihar2000@users.noreply.github.com>
2023-06-26 14:50:16 +0530

    deleted the tour by ID

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -76,3 +76,16 @@ exports.updateTour = async (req, res) => {
     })
   }
 }
+
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail'
+    })
+  }
+}
--- routes/toursRoute.js
+++ routes/toursRoute.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { getTours, theTourId, checkID, checkTheValidity, createTour, updateTour } = require('../controllers/toursController')
+const { getTours, theTourId, checkID, checkTheValidity, createTour, updateTour, deleteTour } = require('../controllers/toursController')
 
+router.param('id', checkID)
 router.get('/', getTours)
 router.get('/:id', theTourId)
-router.param('id', checkID)
 router.post('/createTour', createTour)
 router.patch('/:id', updateTour)
+router.delete('/:id', deleteTour)
 
 module.exports = router
